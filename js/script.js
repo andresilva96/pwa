ec2c172462d9714704d0f7078b832e914898733d
Andre Silva <andre.silva@actualsales.com.br>
2017-10-05 09:45:29 -0300

    segundo commit

--- js/script.js
+++ js/script.js
@@ -20,3 +20,14 @@ $(document).ready(function(){
 		$("#form3").hide();
     });
 });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function(reg) {
+            console.log('Service worker Registered');
+        })
+        .catch(function (err) {
+            console.log('erro', err);
+        });
+}
